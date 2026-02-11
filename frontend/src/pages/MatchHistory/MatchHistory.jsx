65cd4dac70244ca48f3b784530c044f0778639f3
deqex <depexpubg@gmail.com>
2026-02-11 22:00:42 +0100

    Add summoner spells and rune icons to MatchHistory WIP

--- frontend/src/pages/MatchHistory/MatchHistory.jsx
+++ frontend/src/pages/MatchHistory/MatchHistory.jsx
@@ -63,6 +63,47 @@ export default function MatchHistory() {
 
   const DD_CHAMPION_ICON_BASE = 'https://ddragon.leagueoflegends.com/cdn/16.3.1/img/champion';
   const DD_ITEM_ICON_BASE = 'https://ddragon.leagueoflegends.com/cdn/16.3.1/img/item';
+  const DD_SUMMONER_ICON_BASE = 'https://ddragon.leagueoflegends.com/cdn/16.3.1/img/spell';
+  const DD_RUNE_ICON_BASE = 'https://ddragon.leagueoflegends.com/cdn/img';
+
+  const SUMMONER_SPELLS = {
+    1: 'SummonerBoost',       // Cleanse
+    3: 'SummonerExhaust',
+    4: 'SummonerFlash',
+    6: 'SummonerHaste',       // Ghost
+    7: 'SummonerHeal',
+    11: 'SummonerSmite',
+    12: 'SummonerTeleport',
+    13: 'SummonerMana',       // Clarity
+    14: 'SummonerDot',        // Ignite
+    21: 'SummonerBarrier',
+    32: 'SummonerSnowball',
+  };
+
+  const RUNE_ICONS = {
+    // Precision keystones
+    8005: 'perk-images/Styles/Precision/PressTheAttack/PressTheAttack.png',
+    8008: 'perk-images/Styles/Precision/LethalTempo/LethalTempoTemp.png',
+    8010: 'perk-images/Styles/Precision/Conqueror/Conqueror.png',
+    8021: 'perk-images/Styles/Precision/FleetFootwork/FleetFootwork.png',
+    // Domination keystones
+    8112: 'perk-images/Styles/Domination/Electrocute/Electrocute.png',
+    8124: 'perk-images/Styles/Domination/Predator/Predator.png',
+    8128: 'perk-images/Styles/Domination/DarkHarvest/DarkHarvest.png',
+    9923: 'perk-images/Styles/Domination/HailOfBlades/HailOfBlades.png',
+    // Sorcery keystones
+    8214: 'perk-images/Styles/Sorcery/SummonAery/SummonAery.png',
+    8229: 'perk-images/Styles/Sorcery/ArcaneComet/ArcaneComet.png',
+    8230: 'perk-images/Styles/Sorcery/PhaseRush/PhaseRush.png',
+    // Resolve keystones
+    8437: 'perk-images/Styles/Resolve/GraspOfTheUndying/GraspOfTheUndying.png',
+    8439: 'perk-images/Styles/Resolve/VeteranAftershock/VeteranAftershock.png',
+    8465: 'perk-images/Styles/Resolve/Guardian/Guardian.png',
+    // Inspiration keystones
+    8351: 'perk-images/Styles/Inspiration/GlacialAugment/GlacialAugment.png',
+    8360: 'perk-images/Styles/Inspiration/UnsealedSpellbook/UnsealedSpellbook.png',
+    8369: 'perk-images/Styles/Inspiration/FirstStrike/FirstStrike.png',
+  };
 
   const getChampionIconUrl = (name) => {
     if (!name) return null;
@@ -74,6 +115,18 @@ export default function MatchHistory() {
   const getItemIconUrl = (id) => {
     if (!id) return null;
     return `${DD_ITEM_ICON_BASE}/${id}.png`;
+  };
+
+  const getRuneIconUrl = (id) => {
+    const rel = id != null ? RUNE_ICONS[id] : null;
+    if (!rel) return null;
+    return `${DD_RUNE_ICON_BASE}/${rel}`;
+  };
+
+  const getSummonerIconUrl = (id) => {
+    const key = id != null ? SUMMONER_SPELLS[id] : null;
+    if (!key) return null;
+    return `${DD_SUMMONER_ICON_BASE}/${key}.png`;
   };
 
   const SimplifiedMatchCard = ({ match }) => {
@@ -122,6 +175,16 @@ export default function MatchHistory() {
 
     const focusChampionIcon = getChampionIconUrl(focusPlayer?.championName);
 
+    const primaryStyle = focusPlayer?.perks?.styles?.find(s => s.description === 'primaryStyle')
+      || focusPlayer?.perks?.styles?.[0];
+    const primaryKeystoneId = primaryStyle?.selections?.[0]?.perk;
+    const primaryRuneIcon = getRuneIconUrl(primaryKeystoneId);
+
+    const summonerIcons = [
+      getSummonerIconUrl(focusPlayer.summoner1Id),
+      getSummonerIconUrl(focusPlayer.summoner2Id),
+    ];
+
     const itemSlots = [
       focusPlayer.item0,
       focusPlayer.item1,
@@ -152,10 +215,33 @@ export default function MatchHistory() {
 
         {/* Runes Section */}
         <div className="match-runes-section">
+          <div className="rune-slot">
+            {primaryRuneIcon && (
+              <img
+                src={primaryRuneIcon}
+                alt="Keystone rune"
+                className="rune-icon-img"
+              />
+            )}
+          </div>
           <div className="rune-slot"></div>
           <div className="rune-slot"></div>
           <div className="rune-slot"></div>
-          <div className="rune-slot"></div>
+        </div>
+
+        {/* Summoner Spells */}
+        <div className="match-summoners-section">
+          {summonerIcons.map((src, idx) => (
+            <div key={idx} className="summoner-icon">
+              {src && (
+                <img
+                  src={src}
+                  alt="Summoner spell"
+                  className="summoner-icon-img"
+                />
+              )}
+            </div>
+          ))}
         </div>
 
         {/* Champion Portrait */}
